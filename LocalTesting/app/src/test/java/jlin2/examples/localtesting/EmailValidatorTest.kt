57a3481742f078a2b168ebfd572287697e7d5952
Dharshini Panneerchelvam <dharshipc@gmail.com>
2025-04-04 13:16:03 -0400

    Add valid test cases

--- LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.kt
+++ LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.kt
@@ -1,4 +1,16 @@
 package jlin2.examples.localtesting
 
+import org.junit.Assert.*
+import org.junit.Test
+
 class EmailValidatorTest {
+    @Test
+    fun validEmail_basicFormat_returnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.com"))
+    }
+
+    @Test
+    fun validEmail_subdomainFormat_returnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("123@abc.co.ca"))
+    }
 }
