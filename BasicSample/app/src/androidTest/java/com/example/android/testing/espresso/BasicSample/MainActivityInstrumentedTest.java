a0639cb495756b75193bb70c3cd0ddb22ad3e3cb
Harpreet Kaur <harpreet140507@gmail.com>
2025-04-07 15:40:13 -0400

    Adding one unit test TestChangeTextButton_with123

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.java
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.java
@@ -1,4 +1,33 @@
 package com.example.android.testing.espresso.BasicSample;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+@RunWith(AndroidJUnit4.class)
 public class MainActivityInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testTextViewHasCorrectText() {
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("Hello Espresso!")));
+    }
+    @Test
+    public void testChangeTextButton_with123Input() {
+        onView(withId(R.id.editTextUserInput)).perform(typeText("123"), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("123")));
+    }
 }
