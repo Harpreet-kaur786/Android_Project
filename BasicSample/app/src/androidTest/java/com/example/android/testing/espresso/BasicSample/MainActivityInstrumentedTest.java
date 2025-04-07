20a1053fad012f0aaa5411e4123f001fbdf3e755
m-bhalla236346 <monikabhalla1971508@gmail.com>
2025-04-07 17:01:54 -0400

    Add test to verify Open Activity button passes 'abcdef' input to new activity

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.java
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.java
@@ -89,4 +89,12 @@ public class MainActivityInstrumentedTest {
         onView(withId(R.id.changeTextBt)).perform(click());
         onView(withId(R.id.textToBeChanged)).check(matches(withText("abcdef")));
     }
+
+    // ✅ New Test: "abcdef" with Open Activity button
+    @Test
+    public void testOpenActivity_withAbcdefInput() {
+        onView(withId(R.id.editTextUserInput)).perform(typeText("abcdef"), closeSoftKeyboard());
+        onView(withId(R.id.activityChangeTextBtn)).perform(click());
+        onView(withId(R.id.show_text_view)).check(matches(withText("abcdef")));
+    }
 }
